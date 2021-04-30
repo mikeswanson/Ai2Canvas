f0997d842e5ff2230a6bc43387493fe1426a6706
Mike Swanson <mdswanson@comcast.net>
2021-04-30 15:50:58 -0700

    Updates year and version to 1.6

--- Source/AnimationFunction.cpp
+++ Source/AnimationFunction.cpp
@@ -1,6 +1,6 @@
 // AnimationFunction.cpp
 //
-// Copyright (c) 2010-2020 Mike Swanson (http://blog.mikeswanson.com)
+// Copyright (c) 2010-2021 Mike Swanson (http://blog.mikeswanson.com)
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
